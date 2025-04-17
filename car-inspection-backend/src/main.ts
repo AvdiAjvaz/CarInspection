755fde24866f06f577ea6308241fcf684da4f3d7
AvdiAjvaz <avdiajvazi4@gmail.com>
2025-04-17 11:07:40 +0200

    Update main.ts

--- car-inspection-backend/src/main.ts
+++ car-inspection-backend/src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Konfigurim i detajuar i CORS
+ 
   app.enableCors({
-    origin: 'http://localhost:4200', // Lejo vetëm këtë domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // OPTIONS është i rëndësishëm për preflight
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
